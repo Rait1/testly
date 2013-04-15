cdde2a9ca125d7dc55692efed26daefba19ad234
Rait Loos <rait.loos@gmail.com>
2013-04-15 14:43:03 +0300

    Add basic MVC functionality

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require 'classes/Request.php' ?>
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -22,7 +23,7 @@
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
         <!-- Add your site or application content here -->
-        <?php require $_GET['page'].'.php'; ?>
+
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
         <script src="assets/js/plugins.js"></script>
