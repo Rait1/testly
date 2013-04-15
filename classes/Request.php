eff279cb2bb636dfaded05cd34638323eebb3a50
Rait Loos <rait.loos@gmail.com>
2013-04-15 12:58:15 +0300

    It's something!

---
+++ classes/Request.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Rait
+ * Date: 15.04.13
+ * Time: 12:47
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Request {
+	public $controller = DEFAULT_CONTROLLER;
+	public $action = 'index';
+	public $params = array();
+
+
+}
--- index.php
+++ index.php
@@ -15,16 +15,14 @@
         <link rel="stylesheet" href="assets/css/normalize.css">
 	    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
         <link rel="stylesheet" href="assets/css/main.css">
-
         <script src="assets/js/vendor/modernizr-2.6.2.min.js"></script>
     </head>
     <body>
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
-
         <!-- Add your site or application content here -->
-        <p>Hello world! This is HTML5 Boilerplate.</p>
+        <?php require $_GET['page'].'.php'; ?>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
