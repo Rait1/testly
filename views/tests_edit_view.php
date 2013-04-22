a664d4ffcc99be1878b47f498f1527ba89cb9613
Rait Loos <rait.loos@gmail.com>
2013-04-22 14:12:46 +0300

    Improve test edit view.

--- views/tests_edit_view.php
+++ views/tests_edit_view.php
@@ -14,22 +14,88 @@
 	<div id="tabs-1">
 		<form method="post">
 			<label>Küsimuse nimi</label>
-			<input type="text" name="name" value="<?=$test['name']?>">
+			<input type="text" name="name" value="<?= $test['name'] ?>">
 			<label>Sissejuhatus</label>
 			<textarea name="introduction"><?=$test['introduction']?></textarea>
 			<label>Kokkuvõte</label>
 			<textarea name="conclusion"><?=$test['conclusion']?></textarea>
 			<label>Passcode</label>
-			<input type="text" name="passcode" value="<?=$test['passcode']?>">
+			<input type="text" name="passcode" value="<?= $test['passcode'] ?>">
 		</form>
 	</div>
 	<div id="tabs-2">
-		<p>Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis
-			scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros
-			massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros
-			vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere
-			viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere,
-			felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.</p>
+		<label>Küsimus</label>
+		<textarea name="question_text"><?=$test['question_text']?></textarea>
+		<label>Tüüp</label>
+		<select name="type_id" id="type_id">
+			<option value="1">True/False</option>
+			<option value="2" selected="selected">Multiple choice</option>
+			<option value="3">Multiple response</option>
+			<option value="4">Fill in the blank</option>
+		</select>
+
+		<div id="answer-template">
+			<div id="type_id_1" class="answer-template">
+				<label>Sisesta kaks vastust ja märgi ära õige</label>
+				<input type="radio" name="tf.correct" value="0" checked="checked">
+				<textarea name="answer.0">True</textarea>
+				<input type="radio" name="tf.correct" value="1">
+				<textarea name="answer.1">False</textarea>
+			</div>
+			<div id="type_id_2">
+				<label>Sisesta vastuse variandid ja märgi ära õige</label>
+
+				<div id="multiple-choice-options">
+					<div class="answer-option">
+						<input type="radio" name="mc.correct" value="0" checked="checked">
+						<textarea name="mc.answer.0"></textarea>
+					</div>
+					<div class="answer-option">
+						<input type="radio" name="mc.correct" value="1">
+						<textarea name="mc.answer.1"></textarea>
+					</div>
+					<div class="answer-option">
+						<input type="radio" name="mc.correct" value="2">
+						<textarea name="mc.answer.2"></textarea>
+					</div>
+					<div class="answer-option">
+						<input type="radio" name="mc.correct" value="3">
+						<textarea name="mc.answer.3"></textarea>
+					</div>
+				</div>
+			</div>
+			<div id="type_id_3" class="answer-template">
+				<label>Sisesta vastuse varjandid ja märgi ära õiged</label>
+
+				<div id="multiple-response-answer-option">
+					<div class="answer-option">
+						<input type="checkbox" name="mr.correct" value="1">
+						<textarea name="mr.answer.0"></textarea>
+					</div>
+					<div class="answer-option">
+						<input type="checkbox" name="mr.correct" value="1">
+						<textarea name="mr.answer.1"></textarea>
+					</div>
+					<div class="answer-option">
+						<input type="checkbox" name="mr.correct" value="1">
+						<textarea name="mr.answer.2"></textarea>
+					</div>
+					<div class="answer-option">
+						<input type="checkbox" name="mr.correct" value="1">
+						<textarea name="mr.answer.3"></textarea>
+					</div>
+				</div>
+			</div>
+			<div id="type_id_4">
+				<label>Sisesta võimaliked vastuse varjandid (Üks vastus ühte kasti)</label>
+				<div id="fill-in-the-blanke-answer-option">
+					<div class="answer-option">
+						<input type="checkbox" name="fitb.correct" checked="checked" disabled="true">
+						<textarea name="fitb.answer.0"></textarea>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 	<div id="tabs-3">
 		<p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi
@@ -46,4 +112,3 @@
 			vitae lectus hendrerit hendrerit.</p>
 	</div>
 </div>
- 
