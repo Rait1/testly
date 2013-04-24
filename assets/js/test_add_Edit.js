ca3c32d55d42d114a837f783754aaccef5b53f59
Rait Loos <rait.loos@gmail.com>
2013-04-24 09:57:23 +0300

    Edit tests_edit_view. Add tests_add_view. Fix test add button. Translate some things in tests_add_view. Fix tests controller. Add tests_add_edit js.

--- assets/js/test_add_Edit.js
+++ assets/js/test_add_Edit.js
@@ -1,33 +1,63 @@
 var current_type_id = 2;
 
+function addMultipleChoice(){
+	var html=('<div class="answer-option"><input type="radio" name="mc.correct" value="<id>">&nbsp;<textarea name="mc.answer.<id>"></textarea></div>');
+	var id=$('#multiple-choice-options textarea').length;
+	html=html.replace(/<id>/g.id);
+	$('#multiple-choice-options').append(html);
+	return false;
+}
+
+function addMultipleResponse(){
+	var html=('<div class="answer-option"><input type="checkbox" name="mr.correct" value="<id>">&nbsp;<textarea name="mr.answer.<id>"></textarea></div>');
+	var id=$('#multiple-response-answer-option textarea').length;
+	html=html.replace(/<id>/g.id);
+	$('#multiple-response-answer-option').append(html);
+	return false;
+}
+
+function removeMultipleChoice(){
+	if ($('#multiple-choice-options textarea').length){
+		$('#multiple-choice-options .answer-option:last').remove();
+	}
+	return false
+}
+
+function removeMultipleResponse(){
+	if ($('#multiple-response-answer-option textarea').length){
+		$('#multiple-response-answer-option .answer-option:last').remove();
+	}
+	return false
+}
+
 function checkForm() {
-	var elements = $('#type_id_' + current_type_id + 'input[type = checkbox]: not(.shuffle_answers), #type_id_' + current_type_id + 'input[type = radio]:not(#shuffle)');
+	var elements = $('#type_id_' + current_type_id + 'input[type=checkbox]:not(.shuffle_answers),#type_id_' + current_type_id + 'input[type=radio]:not(#shuffle)');
 	var textboxes = $('#type_id_' + current_type_id + 'textarea');
 	for (var i = 0; i < elements.length; i++) {
 		if ($(elements[i]).attr('checked') && $.trim($(textboxes[i]).val()) != "") {
 			return true;
 		}
 	}
-	alert("Palun märgi õige vastus.");
+
+	alert("Palun märgi õige vastus");
 	return false;
 }
-$(document).ready(function () {
-	$('#answer-template .answer-template').hide();
-	$('#type_id_' + current_type_id).show();
-	$('#type_id').bind('click change focus', function (event) {
-		if ($(this).val() != current_type_id) {
-			$('#answer-template .answer-template').hide();
-			current_type_id = $(this).val();
-			$('#type_id_' + current_type_id).show();
+$(function () {
+		$('#answer-template .answer-template').hide();
+		$('#type_id_' + current_type_id).show();
+		$('#type_id').bind('click change focus', function (event) {
+			if ($(this).val() != current_type_id) {
+				$('#answer-template .answer-template').hide();
+				current_type_id = $(this).val();
+				$('#type_id_' + current_type_id).show();
+			}
+		});
+		var list = $('#type_id option');
+		for (var i = 0; i < list - length; i++) {
+			if ($(list[i]).val() == current_type_id) {
+				$(list[i]).attr('selected', 'selected')
+			}
 		}
-	});
-	var list = $('#type_id option');
-	for (var i = 0; i < list.length; i++) {
-		if ($(list[i]).val() == current_type_id) {
-			$(list[i]).attr('selected', 'selected');
-		}
+		$('textarea:first').focus();
 	}
-	$('textarea:first').focus();
-});
-
-
+);
